614b6c85561eba9f47c2c02b2c51ea5a17c23ab6
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2022-10-11 08:58:00 +0200

    :art: update menu css

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let click = document.getElementById("click")
+let span1 = document.getElementById("span1")
+let span2 = document.getElementById("span2")
+let span3 = document.getElementById("span3")
+let span4 = document.getElementById("span4")
+let cross = document.getElementById("cross")
+let links = document.getElementById("links")
+
+click.addEventListener('click', function(){
+  span1.style.width = "100%"
+  span2.style.width = "100%"
+  span3.style.width = "100%"
+  span4.style.width = "100%"
+  cross.style.visibility = "visible"
+  links.style.visibility = "visible"
+  links.style.transitionDelay = "1.2s"
+})
+
+cross.addEventListener('click', function(){
+  span1.style.width = "0%"
+  span2.style.width = "0%"
+  span3.style.width = "0%"
+  span4.style.width = "0%"
+  cross.style.visibility = "hidden"
+  links.style.visibility = "hidden"
+  links.style.transitionDelay = "0s"
+})
