a41f8e0c9971f3313c5ef1670d8223c4adc429af
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2022-11-15 18:23:41 +0100

    :lipstick: first project card / :sparkles: new font

--- js/mobile.js
+++ js/mobile.js
@@ -38,8 +38,10 @@ let cardImg = document.querySelector('.card-img')
 repu.addEventListener('mouseenter', function (){
   body.style.background = '#6D9886';
   cardImg.style.filter = 'saturate(0)'
+  cardImg.style.transform = 'scale(1.1)'
 })
 repu.addEventListener('mouseleave', function (){
   body.style.background = '#202020'
   cardImg.style.filter = 'saturate(1)'
+  cardImg.style.transform = 'scale(1)'
 })
