c5ca597e688c2da27de6db5177cd197083d9221a
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2022-11-15 23:13:41 +0100

    :lipstick: project card

--- js/project.js
+++ js/project.js
@@ -8,9 +8,12 @@ repu.addEventListener('mouseenter', function (){
   body.style.background = '#6D9886';
   cardImg.style.filter = 'saturate(0)'
   cardImg.style.transform = 'scale(1.1)'
+  
+
 })
 repu.addEventListener('mouseleave', function (){
   body.style.background = '#202020'
   cardImg.style.filter = 'saturate(1)'
   cardImg.style.transform = 'scale(1)'
+  
 })
